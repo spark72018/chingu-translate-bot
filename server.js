9987d61c3b10329d9dad2e514e6a0d8e4cd1193c
spark72018 <spark89@gmail.com>
2017-07-23 12:10:27 -0400

    added OAuth

--- server.js
+++ server.js
@@ -38,6 +38,30 @@ languages that work so far:
     - es, de, fr, it, ja, ko, pt (portuguese), 
 */
 
+// OAuth
+app.get('/auth', function(req, res) {
+  var data = {form: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code
+  }};
+  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // Get an auth token
+      var token = JSON.parse(body).access_token;
+
+      // Get the team domain name to redirect to the team URL after auth
+      request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var team = JSON.parse(body).team.domain;
+            res.redirect('http://'+team+'.slack.com');
+          //}
+        }
+      });
+    }
+  })
+});
+
 app.post('/:lang', (req, res) => {
     const targetLanguage = req.params.lang;
     const textToTranslate = req.body.text;
