7a71ac2ffdb4dd52fde7e04675d6d8205db35c97
spark72018 <spark89@gmail.com>
2017-07-23 15:25:50 -0400

    added request package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
 const LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
 const translator = new LanguageTranslatorV2({
